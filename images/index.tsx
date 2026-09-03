6191ee1849a0a8043ca3d14c5bd5978379a5f5fe
Abdulhamid-Azamov <azaovabdulkhamid@gmail.com>
2026-09-03 10:32:42 +0500

    Fix remaining merge conflict

--- images/index.tsx
+++ images/index.tsx
@@ -1,12 +1,13 @@
-<<<<<<< HEAD
 import LogoImage from "./logosite.png"
-=======
-import LogoImage from "./logo.jpg"
->>>>>>> 52fa01cbdd02b7a5ba6c748711dd6ba7f0b280f7
 import HeroImage from "./hero-img.webp"
 import AboutImage from "./about-image.webp"
 import BurgerMenuIcon from "./icons/burger-menu.svg"
 
-const images = { LogoImage, HeroImage, AboutImage, BurgerMenuIcon }
+const images = {
+  LogoImage,
+  HeroImage,
+  AboutImage,
+  BurgerMenuIcon,
+}
 
 export default images
